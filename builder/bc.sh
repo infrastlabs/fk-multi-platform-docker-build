dab5ad22d9e6191d93372829ed5512bcdd82876f
sam#gemmi-win10 <sam@china.gz>
2023-10-02 13:32:19 +0800

    --output type=image,name=test.registry.ssl:8143/bc:v0.1,push=true

--- builder/bc.sh
+++ builder/bc.sh
@@ -4,10 +4,14 @@ FROM registry.cn-shenzhen.aliyuncs.com/infrasync/alpine:3.13.12
 RUN echo 123;  uname -a; 
 EOF
 
+
+# buildctl build ... \
+# --output type=image,\"name=docker.io/username/image,docker.io/username2/image2\",push=true
 cd /tmp
 buildctl build \
     --frontend=dockerfile.v0 \
     --local context=. \
     --local dockerfile=. \
-    --opt target=foo \
-    --opt build-arg:foo=bar
+    --output type=image,name=test.registry.ssl:8143/bc:v0.1,push=true
+    # --opt target=foo \
+    # --opt build-arg:foo=bar
